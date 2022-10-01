6dbaf1320d0cb8ad647c3c50a2908e2e084097a9
Shuvrajeet Das <shuvrajeet17@gmail.com>
2022-10-01 18:24:12 +0530

    updates

--- study/probabilistic_models/probabilistic_regression.py
+++ study/probabilistic_models/probabilistic_regression.py
@@ -16,10 +16,27 @@ model_prob = tf.keras.models.Sequential(
          lambda t: tfp.distributions.Normal(loc=t, scale=1))
      ])
 
+model_prob2 = tf.keras.models.Sequential(
+    [tf.keras.layers.Dense(2, input_shape=(1,)),
+     tfp.layers.DistributionLambda(
+         lambda t: tfp.distributions.Normal(
+             loc=t[..., : 1],
+             scale=tf.math.softplus(t[..., 1:])))
+     ])
+
+model_prob3 = tf.keras.models.Sequential(
+    [tf.keras.layers.Dense(8, input_shape=(1,),
+                           activation='sigmoid'),
+     tf.keras.layers.Dense(
+         tfp.layers.IndependentNormal.params_size(event_shape=1)),
+     tfp.layers.IndependentNormal(event_shape=1)])
+
 
 def nll(y_true, y_pred):
     return -y_pred.log_prob(y_true)
 
+
+optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.01)
 
 if __name__ == '__main__':
     x_train = np.linspace(-1, 1, 100)[:, np.newaxis]
@@ -29,7 +46,7 @@ if __name__ == '__main__':
     plt.xlabel('x')
     plt.ylabel('y')
 
-    model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer='adam')
+    model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer=optimizer)
     model.summary()
     model.fit(x_train, y_train, epochs=500)
 
@@ -42,7 +59,7 @@ if __name__ == '__main__':
     plt.legend()
     plt.show()
 
-    model_prob.compile(loss=nll, optimizer='adam')
+    model_prob.compile(loss=nll, optimizer=optimizer)
     model_prob.summary()
     model_prob.fit(x_train, y_train, epochs=500)
 
@@ -59,13 +76,77 @@ if __name__ == '__main__':
     ax1.scatter(x_train, y_sample, alpha=0.4, label='model_sample', color='r')
     ax1.legend()
     ax2.scatter(x_train, y_train, alpha=0.4, label='data')
-    ax2.plot(x_train, y_hat, color='r', alpha=0.8, label='model $\mu$')
+    ax2.plot(x_train, y_hat, color='r', alpha=0.8, label='model $\\mu$')
     ax2.plot(
         x_train,
         y_hat_m2sd,
         color='g',
         alpha=0.8,
-        label='model $\mu \pm 2 \sigma$')
+        label='model $\\mu \\pm 2 \\sigma$')
+    ax2.plot(x_train, y_hat_p2sd, color='g', alpha=0.8)
+    ax2.legend()
+
+    plt.show()
+
+    model_prob2.compile(loss=nll, optimizer=optimizer)
+    model_prob2.summary()
+    model_prob2.fit(x_train, y_train, epochs=500)
+
+    y_model = model_prob2(x_train)
+    y_sample = y_model.sample()
+    y_hat = y_model.mean()
+    y_sd = y_model.stddev()
+
+    y_hat_m2sd = y_hat - 2*y_sd
+    y_hat_p2sd = y_hat + 2*y_sd
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5), sharey=True)
+    ax1.scatter(x_train, y_train, alpha=0.4, label='data')
+    ax1.scatter(x_train, y_sample, alpha=0.4, label='model_sample', color='r')
+    ax1.legend()
+    ax2.scatter(x_train, y_train, alpha=0.4, label='data')
+    ax2.plot(x_train, y_hat, color='r', alpha=0.8, label='model $\\mu$')
+    ax2.plot(
+        x_train,
+        y_hat_m2sd,
+        color='g',
+        alpha=0.8,
+        label='model $\\mu \\pm 2 \\sigma$')
+    ax2.plot(x_train, y_hat_p2sd, color='g', alpha=0.8)
+    ax2.legend()
+
+    plt.show()
+
+    x_train = np.linspace(-1, 1, 1000)[..., np.newaxis]
+    y_train = np.power(
+        x_train, 3)+0.1*(2+x_train)*np.random.randn(1000)[:, np.newaxis]
+    plt.scatter(x_train, y_train, alpha=0.1)
+    plt.show()
+
+    model_prob3.compile(loss=nll, optimizer=optimizer)
+    model_prob3.summary()
+    model_prob3.fit(x_train, y_train, epochs=500)
+
+    y_model = model_prob3(x_train)
+    y_sample = y_model.sample()
+    y_hat = y_model.mean()
+    y_sd = y_model.stddev()
+
+    y_hat_m2sd = y_hat - 2*y_sd
+    y_hat_p2sd = y_hat + 2*y_sd
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5), sharey=True)
+    ax1.scatter(x_train, y_train, alpha=0.4, label='data')
+    ax1.scatter(x_train, y_sample, alpha=0.4, label='model_sample', color='r')
+    ax1.legend()
+    ax2.scatter(x_train, y_train, alpha=0.4, label='data')
+    ax2.plot(x_train, y_hat, color='r', alpha=0.8, label='model $\\mu$')
+    ax2.plot(
+        x_train,
+        y_hat_m2sd,
+        color='g',
+        alpha=0.8,
+        label='model $\\mu \\pm 2 \\sigma$')
     ax2.plot(x_train, y_hat_p2sd, color='g', alpha=0.8)
     ax2.legend()
 
