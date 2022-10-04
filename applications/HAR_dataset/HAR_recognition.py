d1253a83d7f2d90003267397dbffe99eaedbcd85
Shuvrajeet Das <shuvrajeet17@gmail.com>
2022-10-04 06:28:46 +0530

    updates

---
+++ applications/HAR_dataset/HAR_recognition.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import tensorflow_probability as tfp
+import matplotlib.pyplot as plt
+
+def main():
+    data = pd.read_csv('train.csv')
+    column_names = data.columns
+
+
+if __name__ == '__main__':
+    main()
